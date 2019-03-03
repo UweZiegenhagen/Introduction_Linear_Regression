52adaecb03deee4a83b8899268a90a559ccfd4ef
Uwe Ziegenhagen <UweZiegenhagen@users.noreply.github.com>
2019-03-03 21:01:15 +0100

    some examples adjusted, need better numbers.

--- linreg-01.R
+++ linreg-01.R
@@ -1,5 +1,5 @@
-x = c(1,2,3,4,5)
-y = c(1,3,2,5,4)
+x = c(1,2,3,4,5,6)
+y = c(1,3,2,5,4,5)
 
-model = lm(x~y)
+model = lm(y~x)
 summary(model)
--- linreg-01.py
+++ linreg-01.py
@@ -1,7 +1,7 @@
 from scipy import stats
 
-x = [1, 2, 3, 4, 5]
-y = [1, 3, 2, 5, 4]
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 3, 2, 5, 4, 5]
 
 slope, intercept, r_value, p_value, std_error = stats.linregress(x,y)
 
